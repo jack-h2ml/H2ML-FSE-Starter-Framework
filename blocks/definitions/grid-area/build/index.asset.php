5e307427e3b97bafa7d97191717898ba461342ba
jacknotman <jack@h2ml.agency>
2023-01-15 15:00:20 +0000

    Fixes

--- blocks/definitions/grid-area/build/index.asset.php
+++ blocks/definitions/grid-area/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '4c69cec8c18d9d41b880');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '2bb57fae06482b303e17');
