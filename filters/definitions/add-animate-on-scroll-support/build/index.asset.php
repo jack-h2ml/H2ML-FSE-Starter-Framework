4e1b58619240b5263924fc4159d10ad6ab34090d
jacknotman <jack@h2ml.agency>
2023-02-12 10:38:56 +0000

    Updated Versioning

--- filters/definitions/add-animate-on-scroll-support/build/index.asset.php
+++ filters/definitions/add-animate-on-scroll-support/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '8af776e1ed9664edd8a1');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '07dc589f3e8e40edd02b');
--- filters/definitions/add-animate-on-scroll-support/build/view.asset.php
+++ filters/definitions/add-animate-on-scroll-support/build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '350369bb2e881ea80c52');
+<?php return array('dependencies' => array(), 'version' => '94c0c330f6ef4b8874b4');
