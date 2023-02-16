5b326acd8cdf95baab83c89dce0aa47678ec1470
jacknotman <jack@h2ml.agency>
2023-02-16 00:31:22 +0000

    Animate On Scroll Build

--- filters/definitions/add-animate-on-scroll-support/build/index.asset.php
+++ filters/definitions/add-animate-on-scroll-support/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '43ea19e84a40f47e37e8');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '91505262d210a3c6d5a8');
--- filters/definitions/add-animate-on-scroll-support/build/view.asset.php
+++ filters/definitions/add-animate-on-scroll-support/build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'a19ee791ecb964a946a6');
+<?php return array('dependencies' => array(), 'version' => '3e1012f8aca42a0102f8');
