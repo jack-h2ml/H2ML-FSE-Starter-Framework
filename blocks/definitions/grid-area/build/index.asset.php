e87d910fc37b170921aa2bc44af7268c5b5c67a0
jacknotman <jack@h2ml.agency>
2023-02-18 00:56:11 +0000

    Grid Area Styles

--- blocks/definitions/grid-area/build/index.asset.php
+++ blocks/definitions/grid-area/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '8b6853a218ba5f4f2a20');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '0d9e7ea2a372f468b5ae');
