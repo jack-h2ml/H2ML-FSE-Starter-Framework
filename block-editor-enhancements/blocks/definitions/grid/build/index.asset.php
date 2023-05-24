3b4dc1c9c2bbca1f31dae2abc56ff2b40bd6fc14
jacknotman <jack@h2ml.agency>
2023-05-24 23:47:47 +0100

    Grid Breakpoint Editor Fixes

--- block-editor-enhancements/blocks/definitions/grid/build/index.asset.php
+++ block-editor-enhancements/blocks/definitions/grid/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b3ce374be691dc0cefe2');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '37d09299a0724fabc6d1');
