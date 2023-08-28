86b70e3a04b21f8c30c829b2bcfef9854d85ab54
jacknotman <jack@h2ml.agency>
2023-08-28 10:53:51 +0100

    Grid Area Alignment Fixes

--- block-editor-enhancements/blocks/definitions/grid-area/build/index.asset.php
+++ block-editor-enhancements/blocks/definitions/grid-area/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '639ca68312c4a3f5bc2a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '051ebeca26460a7b1eb9');
