b0c558c9859fe0c741a6acb2cccdd4ea80595221
jacknotman <jack@h2ml.agency>
2023-08-28 10:37:45 +0100

    Grid Area Alignment Fixes

--- block-editor-enhancements/blocks/definitions/grid-area/build/index.asset.php
+++ block-editor-enhancements/blocks/definitions/grid-area/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '99610cae1f544128e0ec');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c8116427231376188947');
--- block-editor-enhancements/blocks/definitions/grid-area/src/save.js
+++ block-editor-enhancements/blocks/definitions/grid-area/src/save.js
@@ -26,6 +26,8 @@ export default function Save({
 		horizontal: 'row'
 	}[layout.orientation];
 
+	console.log('hmm', layout.orientation, flexDirection);
+
 	const blockProps = useBlockProps.save({
 		className: 'alignfull',
 		style: {
