b8411ae99c548d4d75781bfaffeea1f047e8639d
jacknotman <jack@h2ml.agency>
2025-03-06 13:54:41 +0000

    Fixes

--- block-editor-enhancements/blocks/definitions/grid-area/build/index.asset.php
+++ block-editor-enhancements/blocks/definitions/grid-area/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '2f2f68eab908e74f4457');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '369b3e8855b977b4b5f2');
